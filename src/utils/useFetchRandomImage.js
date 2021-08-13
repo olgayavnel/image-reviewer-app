a4a75e2d74a9993d1d09c507b0f451f39c94a428
Olga <olgaiv.berlin@gmail.com>
2021-08-13 18:38:39 +0200

    commit

--- src/utils/useFetchRandomImage.js
+++ src/utils/useFetchRandomImage.js
@@ -4,7 +4,11 @@ import { ENDPOINT } from './constants';
 
 export function useFetchRandomImage() {
   const [isLoading, setIsLoading] = useState(false);
-  const [randomImages, setRandomImages] = useState({});
+  const [randomImages, setRandomImage] = useState({});
+  console.log(
+    '🚀 ~ file: useFetchRandomImage.js ~ line 8 ~ useFetchRandomImage ~ randomImage',
+    randomImages
+  );
 
   /** the useSelector hook lets our component extract whatever pieces of data it needs
    * from the Redux store state.
@@ -19,11 +23,27 @@ export function useFetchRandomImage() {
       .then((response) => {
         return response.json();
       })
-      .then((data) => {})
+      .then((data) => {
+        const imageObjects = clonedData.map((item) => {
+          return { id: item.id, randomImageUrl: item.urls.regular };
+        });
+
+        setIsLoading(false);
+        setRandomImage(imageObjects);
+      })
       .catch((err) => {
         console.log('Error: ' + err);
       });
   }
+
+  // // validation if an image was rejected before
+  // const newId = item.id;
+  // const rejectedImageListIds = rejectedImageList.map((image) => image.id);
+  // if (rejectedImageListIds.includes(newId)) {
+  //   // fetch again
+  //   generateNewRandomImageArray();
+  //   return;
+  // }
 
   // do it right away once
   useEffect(generateNewRandomImageArray, []);
