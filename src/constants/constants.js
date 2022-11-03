33807a3152bcd99780d9718d977d98bc87815db9
Olga Ivanova <54780131+olgayavnel@users.noreply.github.com>
2022-11-03 21:42:13 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = 'QzNmnLqbQYl8KoEjd0G1Mk8bAUil08OqT7z4SIuoH48';
+export const CLIENT_ID = '';
 export const ENDPOINT = `https://api.unsplash.com/photos/random/?client_id=${CLIENT_ID}`;
